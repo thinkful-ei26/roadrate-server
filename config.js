8f09f747728a0d4720af9a1c305472f096d71987
RP Boyle <rpboyle11@yahoo.com>
2019-02-22 16:18:00 -0500

    deleted some data from config.js

--- config.js
+++ config.js
@@ -10,9 +10,4 @@ module.exports = {
         'mongodb://localhost/thinkful-backend-test',
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY || '7d' 
-  // DATABASE_URL:
-  //     process.env.DATABASE_URL || 'postgres://localhost/thinkful-backend',
-  // TEST_DATABASE_URL:
-  //     process.env.TEST_DATABASE_URL ||
-  //     'postgres://localhost/thinkful-backend-test'
 };

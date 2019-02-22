7cafe779e15b25e5245f5ae32cd70e0e05679af3
RP Boyle <rpboyle11@yahoo.com>
2019-02-22 16:16:57 -0500

    added mongodb

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   DATABASE_URL:
-        process.env.DATABASE_URL || 'mongodb://localhost/thinkful-backend',
+        process.env.DATABASE_URL || 'mongodb://jordan:haddadi@roadrate-shard-00-00-qquzy.mongodb.net:27017,roadrate-shard-00-01-qquzy.mongodb.net:27017,roadrate-shard-00-02-qquzy.mongodb.net:27017/test?ssl=true&replicaSet=RoadRate-shard-0&authSource=admin&retryWrites=true',
   TEST_DATABASE_URL:
         process.env.TEST_DATABASE_URL ||
         'mongodb://localhost/thinkful-backend-test',
